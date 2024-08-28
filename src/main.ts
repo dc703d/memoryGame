0edea68754eecf5c984bc26f599eea6a6adf48ee
Daim <daim_choudhary@yahoo.co.uk>
2024-08-28 14:43:58 +0100

    added the fisherYates shuffle to shuffle the cards

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './style.css';
+
+const symbols: string[] = ['🏀', '🏀', '🎮', '🎮', '🧩', '🧩', '🀄', '🀄', '🎯', '🎯', '🥊', '🥊', '🎱', '🎱', '🎳', '🎳','🦍','🦍'];
+
+function fisherYatesShuffle(array: string[]) {
+  for (let i = array.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const shuffledSymbols: string[] = fisherYatesShuffle(symbols);
